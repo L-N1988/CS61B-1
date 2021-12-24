8de386db128367f28a00d0e369ae4a0da6d5ec5a
Christina0031 <781364740@qq.com>
2021-12-24 23:33:08 +0800

    finished implementing the physics

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.hypot;
+
 public class Planet {
     public double xxPos;
     public double yyPos;
@@ -5,6 +7,7 @@ public class Planet {
     public double yyVel;
     public double mass;
     String imgFileName;
+    static final double G = 6.67e-11;
 
     public Planet(double xP, double yP, double xV,
                   double yV, double m, String img) {
@@ -17,13 +20,61 @@ public class Planet {
     }
 
     public Planet(Planet p) {
-        this.xxPos = p.xxPos;
-        this.yyPos = p.yyPos;
-        this.xxVel = p.xxVel;
-        this.yyVel = p.yyVel;
-        this.mass = p.mass;
-        this.imgFileName = p.imgFileName;
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
     }
 
+    public double calcDistance(Planet p) {
+        return hypot(xxPos - p.xxPos, yyPos - p.yyPos);
+    }
 
+    public double calcForceExertedBy(Planet p) {
+        double distance = this.calcDistance(p);
+        return G * mass * p.mass / (distance * distance);
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        double force = this.calcForceExertedBy(p);
+        double distance = this.calcDistance(p);
+        return (p.xxPos - xxPos) * force / distance;
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        double force = this.calcForceExertedBy(p);
+        double distance = this.calcDistance(p);
+        return (p.yyPos - yyPos) * force / distance;
+    }
+
+    public double calcNetForceExertedByX(Planet[] plants) {
+        double net_force = 0;
+        for (Planet planet : plants) {
+            if (!this.equals(planet)) {
+                net_force += this.calcForceExertedByX(planet);
+            }
+        }
+        return net_force;
+    }
+
+    public double calcNetForceExertedByY(Planet[] plants) {
+        double net_force = 0;
+        for (Planet planet : plants) {
+            if (!this.equals(planet)) {
+                net_force += this.calcForceExertedByY(planet);
+            }
+        }
+        return net_force;
+    }
+
+    public void update(double time, double x_force, double y_force) {
+        double acceleration_x = x_force / mass;
+        double acceleration_y = y_force / mass;
+        xxVel += acceleration_x * time;
+        yyVel += acceleration_y * time;
+        xxPos += xxVel * time;
+        yyPos += yyVel * time;
+    }
 }
