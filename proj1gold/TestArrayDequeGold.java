1ea8dc9e678332aa9636cde76cd8c2c75dd6678f
Christina0031 <781364740@qq.com>
2022-01-05 21:34:24 +0800

    Proj1Gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -12,10 +12,10 @@ public class TestArrayDequeGold {
         for (int i = 0; i < 400; i += 1) {
             double randomNumber = StdRandom.uniform();
             if (randomNumber < 0.5) {
-                if (!sol.isEmpty() && randomNumber < 0.25) {
+                if (!sol.isEmpty() && !sad.isEmpty() && randomNumber < 0.25) {
                     Integer actual = sad.removeLast();
                     Integer expected = sol.removeLast();
-                    String methodCall = "removeLast(): " + actual + "\n";
+                    String methodCall = "removeLast()\n";
                     errorMessage = errorMessage + methodCall;
                     assertEquals(errorMessage, expected, actual);
                 } else {
@@ -25,10 +25,10 @@ public class TestArrayDequeGold {
                     errorMessage = errorMessage + methodCall;
                 }
             } else {
-                if (!sol.isEmpty() && randomNumber < 0.75) {
+                if (!sol.isEmpty() && !sad.isEmpty() && randomNumber < 0.75) {
                     Integer actual = sad.removeFirst();
                     Integer expected = sol.removeFirst();
-                    String methodCall = "removeFirst(): " + actual + "\n";
+                    String methodCall = "removeFirst()\n";
                     errorMessage = errorMessage + methodCall;
                     assertEquals(errorMessage, expected, actual);
                 } else {
