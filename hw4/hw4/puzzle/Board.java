b94a5edc617bae6f219e9805a25e02dd45f79e1c
Christina0031 <781364740@qq.com>
2022-01-29 12:55:14 +0800

    hw04

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,21 +1,62 @@
 package hw4.puzzle;
 
 public class Board {
-
-    /** Returns the string representation of the board. 
-      * Uncomment this method. */
-    /*public String toString() {
-        StringBuilder s = new StringBuilder();
-        int N = size();
-        s.append(N + "\n");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", tileAt(i,j)));
-            }
-            s.append("\n");
-        }
-        s.append("\n");
-        return s.toString();
-    }*/
+//     implements WorldState
+//    /**
+//     * Constructs a board from an N-by-N array of tiles where
+//     * tiles[i][j] = tile at row i, column j
+//     */
+//    public Board(int[][] tiles)
+//
+//    /**
+//     * Returns value of tile at row i, column j (or 0 if blank)
+//     */
+//    public int tileAt(int i, int j)
+//
+//    /**
+//     * Returns the board size N
+//     */
+//    public int size()
+//
+//    /**
+//     * Returns the neighbors of the current board
+//     */
+//    public Iterable<WorldState> neighbors()
+//    /**
+//     * Hamming estimate
+//     */
+//    public int hamming()
+//    /**
+//     * Manhattan estimate
+//     */
+//    public int manhattan()
+//    /**
+//     * Estimated distance to goal. This method should
+//     * simply return the results of manhattan() when submitted to
+//     * Gradescope.
+//     */
+//    public int estimatedDistanceToGoal()
+//    /**
+//     * Returns true if this board's tile values are the same
+//     * position as y's
+//     */
+//    public boolean equals(Object y)
+//
+//    /**
+//     * Returns the string representation of the board.
+//     */
+//    public String toString() {
+//        StringBuilder s = new StringBuilder();
+//        int N = size();
+//        s.append(N + "\n");
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < N; j++) {
+//                s.append(String.format("%2d ", tileAt(i,j)));
+//            }
+//            s.append("\n");
+//        }
+//        s.append("\n");
+//        return s.toString();
+//    }
 
 }
